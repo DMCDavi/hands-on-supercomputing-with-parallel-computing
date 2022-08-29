a68051237b59a9571d11a8faff071205b8a06e9a
Davi Costa <davi.machado.costa@hotmail.com>
2022-08-29 17:42:49 -0300

    Paralelizando o código de números primos

--- Hands-On-2-Parallelization-with-OpenMP/material/session-3/big_prime.c
+++ Hands-On-2-Parallelization-with-OpenMP/material/session-3/big_prime.c
@@ -1,3 +1,4 @@
+%%writefile big_prime.c
 /*
 File:           big_prime.c
 Last changed:   20220303 11:35:00
@@ -12,26 +13,45 @@ HowToExecute:   OMP_NUM_THREADS=${num_threads} ./big_prime
 #include <stdio.h>
 #include <math.h>
 #include <limits.h>
+#include <omp.h>
 
 typedef unsigned long long big_integer;
 #define BIGGEST_INTEGER ULLONG_MAX
+#define NUM_THREADS 2
 
 int is_prime(big_integer n)
 {
   int result;
-  big_integer sq_root;
+  big_integer sq_root, i;
 
   result = (n % 2 != 0 || n == 2);
 
   if (result)
   {
     sq_root = sqrt(n);
+    
+    omp_set_num_threads(NUM_THREADS);
+    #pragma omp parallel private(i)
+    {
+      int id = omp_get_thread_num();
 
-	// Quando é redução a paralelização é com reduction.
-	// Usar -pg para gerar relatorio na execução e gprof para converter o relatorio
-	#pragma omp parallel for private(i)
-    for (big_integer i = 3; result && i <= sq_root; i += 2)
-      result = n % i != 0;
+      if(id == 0)
+      {
+        i = 3;
+        while(result && i <= (big_integer)sq_root/NUM_THREADS){
+          result = n % i != 0;
+          i += 2;
+        }
+      }
+      if(id == 1)
+      {
+        i = (big_integer)sq_root/NUM_THREADS;
+        while(result && i <= 2*sq_root/NUM_THREADS){
+          result = n % i != 0;
+          i += 2;
+        }
+      }
+    }
   }
 
   return result;
@@ -40,11 +60,13 @@ int is_prime(big_integer n)
 int main(int argc, char **argv)
 {
   big_integer n;
-
+  double t1, t2;
+  t1 = omp_get_wtime();
   for (n = BIGGEST_INTEGER; !is_prime(n); n -= 2)
   {
   }
-
+  t2 = omp_get_wtime();
+  printf("Tempo de execução: %lf\n", t2-t1);
   printf("%llu\n", n);
 
   return 0;
