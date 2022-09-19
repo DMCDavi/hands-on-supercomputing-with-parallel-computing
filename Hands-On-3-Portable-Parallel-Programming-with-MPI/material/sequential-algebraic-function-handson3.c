582967c0d31806ce6425b2b4afb6e3883d832e84
Davi Costa <davi.machado.costa@hotmail.com>
2022-09-19 19:01:38 -0300

    Finalizando hands 3

--- Hands-On-3-Portable-Parallel-Programming-with-MPI/material/sequential-algebraic-function-handson3.c
+++ Hands-On-3-Portable-Parallel-Programming-with-MPI/material/sequential-algebraic-function-handson3.c
@@ -1,35 +1,96 @@
-/*
-@(#)File:           $sequential-algebraic-function-handson3.c$
-@(#)Last changed:   $Date: 20220407 11:35:00 $
-@(#)Purpose:        Algebraic value applied in a function in C
-@(#)Author:         muriloboratto 'at' fieb.org.br
-@(#)Usage:
-@(*) Hotocompile:   gcc sequential-algebraic-function-handson3.c -o obj
-@(*) Hotoexecute:   ./obj
-*/
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
-int main (int argc, char **argv){
+#define MASTER 0
 
-  double coefficient[4], total, x;
+int main(int argc, char ** argv) {
+
+  int numtasks, taskid, dest, source, i, tag1 = 1000, tag2 = 2000, coefficient_position;
+  double coefficient[4], x, suma, sumatotal = 0;
   char c;
 
-  printf ("\nf(x) = a*x^3 + b*x^2 + c*x + d\n");
+  /*-------------- INITIALIZE MPI -----------------*/
+  MPI_Init( &argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+  MPI_Status status;
+  MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
+  /*-----------------------------------------------*/
 
-  for(c = 'a'; c < 'e'; c++) {
-    printf ("\nEnter the value of the 'constants' %c:\n", c);
-    scanf ("%lf", &coefficient[c - 'a']);
+  if (taskid == MASTER) {
+    printf("\nf(x) = a*x^3 + b*x^2 + c*x + d\n");
+
+    /*-------------- CREATE FUNCTION -----------------*/
+    /* ESSA PARTE FOI COMENTADA PORQUE NO COLAB O SCANF DENTRO DE UM LOOP COM MPI NÃO FUNCIONA
+    for (c = 'a'; c < 'e'; c++) {
+      printf("\nEnter the value of the 'constants' %c:\n", c);
+      scanf("%lf", &coefficient[c - 'a']);
+    }
+
+    printf("\nEnter the value of 'x':\n");
+    scanf("%lf", &x);
+    */
+
+    coefficient[0] = 1.0;
+    coefficient[1] = 2.0;
+    coefficient[2] = 3.0;
+    coefficient[3] = 4.0;
+    x = 5.0;
+
+    printf("\nf(%lf) = %lf*x^3 + %lf*x^2 + %lf*x + %lf\n", x, coefficient[0], coefficient[1], coefficient[2], coefficient[3]);
+    /*------------------------------------------------*/
+
+
+    /*-------------- MASTER SEND TO WORKERS -----------------*/
+    for (i = 1; i < numtasks; i++){
+      dest = i;
+      coefficient_position = i - 1;
+
+      MPI_Send( &coefficient[coefficient_position], 1, MPI_DOUBLE, dest, tag1, MPI_COMM_WORLD);
+      MPI_Send( &x, 1, MPI_DOUBLE, dest, tag2, MPI_COMM_WORLD);
+      if (i == 3) {
+        MPI_Send( &coefficient[3], 1, MPI_DOUBLE, dest, tag1, MPI_COMM_WORLD);
+      }
+    }
+
+    /*-------------- MASTER RECEIVE FROM WORKERS -----------------*/
+    for (i = 1; i < numtasks; i++) {
+      source = i;
+      MPI_Recv( &suma, 1, MPI_DOUBLE, source, 3, MPI_COMM_WORLD, &status);
+      sumatotal += suma;
+      printf("Sum from worker-%d = %lf\n", source, suma);
+    }
+
+    printf("Result = %lf\n", sumatotal);
   }
 
-  printf("\nf(x) = %lf*x^3 + %lf*x^2 + %lf*x + %lf\n", coefficient[0], coefficient[1], coefficient[2], coefficient[3]);
+  /*-------------- WORKERS -----------------*/
+  if (taskid > MASTER) {
+    dest = MASTER;
+    source = MASTER;
+    coefficient_position = taskid - 1;
 
-  printf("\nEnter the value of 'x':\n");
-  scanf("%lf", &x);
+    /*-------------- WORKER RECEIVE FROM MASTER -----------------*/
+    MPI_Recv( &coefficient[coefficient_position], 1, MPI_DOUBLE, source, tag1, MPI_COMM_WORLD, &status);
+    MPI_Recv( &x, 1, MPI_DOUBLE, source, tag2, MPI_COMM_WORLD, &status);
+    /*-----------------------------------------------------------*/
 
-  total = (coefficient[0]* x * x * x) + (coefficient[1]* x * x) + (coefficient[2]* x + coefficient[3]);
+    suma = coefficient[coefficient_position] * x;
 
-  printf("\nf(%lf) = %lf*x^3 + %lf*x^2 + %lf*x + %lf = %lf\n", x, coefficient[0], coefficient[1], coefficient[2], coefficient[3], total);
+    /*-------------- WORKERS OPERATIONS -----------------*/
+    if (taskid == 1) {
+      suma *= x * x;
+    } else if (taskid == 2) {
+      suma *= x;
+    } else if (taskid == 3) {
+      MPI_Recv( &coefficient[3], 1, MPI_DOUBLE, source, tag1, MPI_COMM_WORLD, &status);
+      suma += coefficient[3];
+    }
 
+    /*-------------- WORKER SEND TO MASTER -----------------*/
+    MPI_Send( &suma, 1, MPI_DOUBLE, dest, 3, MPI_COMM_WORLD);
+  }
+
+  MPI_Finalize();
   return 0;
 }
--- Hands-On-3-Portable-Parallel-Programming-with-MPI/material/sequential-basic-operations-handson3.c
+++ Hands-On-3-Portable-Parallel-Programming-with-MPI/material/sequential-basic-operations-handson3.c
@@ -1,37 +1,67 @@
-/*
-@(#)File:           $sequential-basic-operations-handson3.c$
-@(#)Last changed:   $Date: 20220407 11:35:00 $
-@(#)Purpose:        Basic operations in a sequential algorithm in C
-@(#)Author:         muriloboratto 'at' fieb.org.br
-@(#)Usage:
-@(*) Hotocompile:   gcc sequential-basic-operations-handson3.c -o obj
-@(*) Hotoexecute:   ./obj
-*/
-
 #include <stdio.h>
+#include <mpi.h>
+
 #define SIZE 12
 
-int main (int argc, char **argv){
-
-  int i, sum = 0, subtraction = 0, mult = 1;
+int main(int argc, char **argv){
   int array[SIZE];
+  char operations[] = {'+', '-', '*'};
+  char operationsRec;
+  int numberOfProcessors, id, to, from, tag = 1000;
+  int i, result, value;
 
-  for(i = 0; i < SIZE; i++)
-    array[i] = i + 1;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcessors);
+  MPI_Comm_rank(MPI_COMM_WORLD, &id);
+  MPI_Status status;
 
-  for(i = 0; i < SIZE; i++)
-    printf("array[%d] = %d\n", i, array[i]);
+  switch (id) {
+    case 0:
+      for(i = 0; i < SIZE; i++){
+        array[i] = i + 1;
+        printf("%d\t", array[i]);
+      }
+      printf("\n");
+      
+      for(to = 1; to < numberOfProcessors; to++) { 
+        MPI_Send(&array, SIZE, MPI_INT, to, tag, MPI_COMM_WORLD);
+        MPI_Send(&operations[to-1], 1, MPI_CHAR, to, tag, MPI_COMM_WORLD);
+      }
+      
+      for(to = 1; to < numberOfProcessors; to++) {
+	    MPI_Recv(&result, 1, MPI_INT, to, tag, MPI_COMM_WORLD, &status);
+	    MPI_Recv(&operationsRec, 1, MPI_CHAR, to, tag, MPI_COMM_WORLD, &status);
+	    printf ("(%c) = %d\n", operationsRec, result);
+	  }
+    break;
+    default:
+      MPI_Recv(&array, SIZE, MPI_INT, 0, tag, MPI_COMM_WORLD, &status);
+      MPI_Recv(&operationsRec, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD, &status);
 
-  for(i = 0; i < SIZE; i++) {
-    sum = sum + array[i];
-    subtraction = subtraction - array[i];
-    mult = mult * array[i];
+      switch (operationsRec) {
+        case '+':
+          value = 0;
+          for(i = 0; i < SIZE; i++)
+            value += array[i];
+          break;
+        
+        case '-':
+          value = 0;
+          for(i = 0; i < SIZE; i++)
+            value -= array[i];
+          break;
+
+        case '*':
+          value = 1;
+          for(i = 0; i < SIZE; i++)
+            value *= array[i];
+          break;
+      }
+
+	  MPI_Send(&value, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    MPI_Send(&operationsRec, 1, MPI_CHAR, 0, tag, MPI_COMM_WORLD);
   }
-
-  printf("Sum = %d\n", sum);
-  printf("Subtraction = %d\n", subtraction);
-  printf("Multiply = %d\n", mult);
-
+  MPI_Finalize();
   return 0;
-
+  
 }
